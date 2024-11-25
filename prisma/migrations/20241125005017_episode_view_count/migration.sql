1041ea3d76704ff586f4bb8c2dd6f8b811d36b9b
Jongho Hong <myodan@pm.me>
2024-11-25 15:49:57 +0900

    🔧 `Webtoon(totalView)`, `Episode(view)` 컬럼 추가

---
+++ prisma/migrations/20241125005017_episode_view_count/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "episode" ADD COLUMN     "view" INTEGER NOT NULL DEFAULT 0;
---
+++ prisma/migrations/20241125005618_webtoon_total_view/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "webtoon" ADD COLUMN     "totalView" INTEGER NOT NULL DEFAULT 0;
