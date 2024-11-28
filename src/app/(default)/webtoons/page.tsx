1628a177cdba3e47f0ffd3caf0a8a2266b5ee778
Jongho Hong <myodan@pm.me>
2024-11-28 15:39:38 +0900

    🎨 알림 사항 추가

--- src/app/(default)/webtoons/page.tsx
+++ src/app/(default)/webtoons/page.tsx
@@ -7,6 +7,7 @@ import {
 	Text,
 } from "@chakra-ui/react";
 import type { FC } from "react";
+import { Alert } from "~/components/ui/alert";
 import { WebtoonList } from "~/components/webtoons/list";
 import { prisma } from "~/lib/prisma";
 
@@ -41,6 +42,10 @@ const WebtoonsPage: FC<WebtoonsPageProps> = async ({ searchParams }) => {
 
 	return (
 		<Stack separator={<StackSeparator />}>
+			<Alert
+				status="error"
+				title="본 사이트는 학교 과제용 사이트입니다. 실제로 운영되지 않습니다."
+			/>
 			<HStack justifyContent="space-between">
 				{!tags && <Heading>전체 웹툰</Heading>}
 				{tags && <Heading>{tags.join(", ")} 웹툰</Heading>}
